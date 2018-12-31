c88bcd987689e25de53de00e436ca7f5e00ca2de
jason98wang <38818672+jason98wang@users.noreply.github.com>
2018-12-31 11:36:26 -0500

    asd

--- src/Lobby.java
+++ src/Lobby.java
@@ -1,8 +1,9 @@
 public class Lobby {
 	
 	public static void main(String [] args) {
-		new GamePanel();
-		new Spinner();
-		Spinner.getRoll();
+//		new GamePanel();
+//		new Spinner();
+//		Spinner.getRoll();
+		new MainMenu();
 	}
 }
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,2 +1,38 @@
-public class MainMenu {
+import java.awt.Font;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class MainMenu extends JFrame{
+	
+	private int numPlayer; 
+    private boolean singlePlayer;
+	
+	MainMenu(){
+		super("Main Menu");
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1200, 1200);
+		
+		this.requestFocusInWindow();
+		
+		this.setVisible(true);
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		mainPanel.setSize(this.getSize());
+		this.setContentPane(mainPanel);
+		
+		
+		JLabel title = new JLabel("Life of Life");
+		title.setAlignmentX(JTextArea.CENTER_ALIGNMENT);
+		title.setHorizontalAlignment(JLabel.CENTER);
+		title.setFont(new Font("Arial",Font.BOLD,50));
+
+		mainPanel.add(title);
+
+	}
 }
