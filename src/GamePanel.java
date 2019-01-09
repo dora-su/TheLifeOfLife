7715e13dd7f87327895f6c1b040a9f798439af35
jason wang <jason98wang@hotmail.com>
2019-01-09 10:49:52 -0500

    movement

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,13 +1,17 @@
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.Graphics;
+import java.awt.Image;
 
 class GamePanel extends JFrame { 
 
   //class variables
   JPanel gamePanel;
+  
+  Image map;
 
   //Constructor - this runs first
    GamePanel() { 
@@ -15,7 +19,8 @@ class GamePanel extends JFrame {
 
     // Set the frame to full screen 
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+    this.setSize(1280,720);
+   
 
     //Set up the game panel (where we put our graphics)
     gamePanel = new GameAreaPanel();
@@ -25,7 +30,15 @@ class GamePanel extends JFrame {
     //this.setUndecorated(true);
     
     this.setVisible(true);
+    
+    map = Toolkit.getDefaultToolkit().getImage("boardSize.png");
+    
+    MyMouseListener mouseListener =  new MyMouseListener();
+    this.addMouseListener(mouseListener);
   
+    
+    //TILE LOCATION
+    
   } //End of Constructor
 
 
@@ -35,8 +48,47 @@ class GamePanel extends JFrame {
     public void paintComponent(Graphics g) {   
        super.paintComponent(g); //required
        setDoubleBuffered(true); 
+       //System.out.println("HI");
+       g.drawImage(map,0,0,null,this);
        repaint();
+       
     }
+  }
+  
+  
+  private class MyMouseListener implements MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("Mouse Clicked");
+		System.out.println("X: " + e.getX() + " Y: " + e.getY());
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	  
   }
   
      
--- src/Lobby.java
+++ src/Lobby.java
@@ -1,9 +1,9 @@
 public class Lobby {
 	
 	public static void main(String [] args) {
-//		new GamePanel();
+		new GamePanel();
 //		new Spinner();
 //		Spinner.getRoll();
-		new MainMenu();
+		//new MainMenu();
 	}
 }
