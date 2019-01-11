90aa2768c42407cb7ee22742aa216806eca94cc5
ericlong2 <ericlong568@gmail.com>
2019-01-11 11:31:03 -0500

    move

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,14 +1,17 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.awt.Graphics;
 import java.awt.Image;
-
 
 class GamePanel extends JFrame {
 
@@ -29,6 +32,19 @@ class GamePanel extends JFrame {
 		this.setSize(1280, 720);
 
 		// Set up the game panel (where we put our graphics)
+		path = new ArrayList<ActionTile>();
+
+		path.add(new ActionTile(26, 317, null, 0));
+		path.add(new ActionTile(75, 317, "HI", 20));
+		path.add(new ActionTile(126, 317, "B", 0));
+		path.add(new ActionTile(173, 299, "D", 0));
+		path.add(new ActionTile(183, 256, "D", 0));
+
+		map = Toolkit.getDefaultToolkit().getImage("graphics/boardSize.jpg");
+		mango1 = Toolkit.getDefaultToolkit().getImage("graphics/mango1.png");
+		
+		player1 = new Player("Eric", 5000, 26, 317);
+
 		gamePanel = new GameAreaPanel();
 		this.add(new GameAreaPanel());
 
@@ -37,31 +53,16 @@ class GamePanel extends JFrame {
 
 		this.setVisible(true);
 
-		map = Toolkit.getDefaultToolkit().getImage("graphics/boardSize.jpg");
-		mango1 = Toolkit.getDefaultToolkit().getImage("graphics/mango1.png");
-
 		MyMouseListener mouseListener = new MyMouseListener();
 		this.addMouseListener(mouseListener);
 
-		player1 = new Player("Eric", 5000, 26, 317);
+		
 
 		// TILE LOCATION
 		// First Path
 		/*
 		 * X: 26 Y: 339 X: 74 Y: 339 X: 129 Y: 339 X: 178 Y: 331
 		 */
-		
-		path = new ArrayList<ActionTile>();
-
-		path.add(new ActionTile(26,317,null,0));
-		path.add(new ActionTile(75,317,"HI",20));
-		path.add(new ActionTile(126,317,"B",0));
-		path.add(new ActionTile(173,299,"D",0));
-		path.add(new ActionTile(183,256,"D",0));
-		
-		
-		
- 
 
 	} // End of Constructor
 
@@ -72,8 +73,8 @@ class GamePanel extends JFrame {
 			super.paintComponent(g); // required
 			setDoubleBuffered(true);
 			// System.out.println("HI");
-			g.drawImage(map, 0, 0, null, this);
-			g.drawImage(mango1, path.get(player1.getTile()).getX() - 17, path.get(player1.getTile()).getY() - 17, null, this);
+			g.drawImage(map, 0, 0, null);
+			g.drawImage(mango1, path.get(player1.getTile()).getX() - 17, path.get(player1.getTile()).getY() - 17, null);
 
 			repaint();
 
@@ -85,35 +86,59 @@ class GamePanel extends JFrame {
 		Scanner input = new Scanner(System.in);
 		int i;
 		for (i = 0; i < spin; i++) {
-			if (path.get(player.getTile()).getIndex() > 0) {
-				
 
-				System.out.println("Choices");
-				//popUp(path.get(player.getTile()).getMessage());
-				
-				return;
-			
-				
-			}
 			if (path.get(player.getTile()).getIndex() == 0) {
 				player.setTile(player.getTile() + 1);
 				gamePanel.repaint();
-				
 			}
+
+			if (path.get(player.getTile()).getIndex() > 0) {
+				break;
+			}
+
+		}
+		if (path.get(player.getTile()).getIndex() > 0) {
+			popUp(path.get(player.getTile()).getMessage(), false);
 		}
 	}
-	
-	private void popUp(String message) {
-		 JFrame popUp = new JFrame();
-		 popUp.setUndecorated(false);
-		 popUp.setResizable(true);
-		 popUp.setVisible(true);
 
-		 //popUp.
-		 System.out.println("HI");
-		 popUp.setSize(120, 120);
+	boolean pop;
+
+	private void popUp(String message, boolean choice) {
+		if (pop) {
+			return;
+		}
+		pop = true;
+		JFrame popUp = new JFrame();
+		popUp.setUndecorated(true);
+		popUp.setResizable(true);
+
+		popUp.setAlwaysOnTop(true);
+		popUp.setLocation(640 - 200, 360 - 100);
+		popUp.setSize(400, 200);
+		popUp.setVisible(true);
+
+		JPanel panel = new JPanel();
+		popUp.add(panel);
+		
+		panel.add(new JLabel(message));
+		if (choice) {
+
+		} else {
+			JButton close = new JButton("Close");
+			close.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					popUp.dispose();
+					pop = false;
+
+				}
+			});
+
+			popUp.add(close);
+		}
+
 	}
-
 
 	private class MyMouseListener implements MouseListener {
 
@@ -121,34 +146,32 @@ class GamePanel extends JFrame {
 		public void mouseClicked(MouseEvent e) {
 			System.out.println("X: " + e.getX() + " Y: " + e.getY());
 
-			move(player1,1);
+			move(player1, 1);
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-
-
 
 	}
 
