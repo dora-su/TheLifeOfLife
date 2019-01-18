26468a724e402c3a928e493dfdaf8623db9602af
jason98wang <38818672+jason98wang@users.noreply.github.com>
2019-01-17 23:21:12 -0500

    asd

--- src/Player.java
+++ src/Player.java
@@ -6,17 +6,18 @@ import javax.swing.Timer;
 
 public class Player {
 
-    private String name;
-    private Career career;
-    private ArrayList<Property> property;
-    private int child;
-    private int tile;
-    private int money;
-    private int destination;
-    private Player player;
-    private boolean college;
-    private int add;
-    private int count;
+	private String name;
+	private Career career;
+	private ArrayList<Property> property;
+	private int child;
+	private int tile;
+	private int money;
+	private int destination;
+	private Player player;
+	private boolean college;
+	private int add;
+	private int count;
+
 	Player(String name, double balance, int x, int y) {
 		this.name = name;
 		tile = 0;
@@ -27,82 +28,88 @@ public class Player {
 		count = 0;
 	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public ArrayList<Property> getProperty() {
-        return property;
-    }
+	public ArrayList<Property> getProperty() {
+		return property;
+	}
 
-    public void addProperty(Property property) {
-        this.property.add(property);
-    }
+	public void addProperty(Property property) {
+		this.property.add(property);
+	}
 
-    public int getChild() {
-        return child;
-    }
+	public int getChild() {
+		return child;
+	}
 
-    public void setChild(int child) {
-        this.child = child;
-    }
+	public void setChild(int child) {
+		this.child = child;
+	}
 
+	public int getTile() {
+		return tile;
+	}
 
-    public int getTile() {
-        return tile;
-    }
+	public void setTile(int tile) {
+		this.tile = tile;
+	}
 
-
-    public void setTile(int tile) {
-        this.tile = tile;
-    }
-    
 	public int getMoney() {
 		return money;
 	}
 
-
 	public void setMoney(int money) {
 		this.money = money;
 	}
-	
+
 	public void addMoney(int money) {
 		add += money;
 		new Timer(1, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (add > 0) {
-					add--;
-					player.setMoney(player.getMoney() + 1);
-				} else if (add < 0) {
-					add++;
-					player.setMoney(player.getMoney() - 1);
+
+				if (money < 500) {
+					if (add > 0) {
+						add--;
+						player.setMoney(player.getMoney() + 1);
+					} else if (add < 0) {
+						add++;
+						player.setMoney(player.getMoney() - 1);
+					}
+				} else {
+					if (add > 0) {
+						add--;
+						player.setMoney(player.getMoney() + 1);
+					} else if (add < 0) {
+						add++;
+						player.setMoney(player.getMoney() - 1);
+					}
 				}
 			}
 		}).start();
 	}
-	
+
 	public void removeMoney(int money) {
 		addMoney(-money);
 	}
-
 
 	public Career getCareer() {
 		return career;
 	}
 
-
 	public void setCareer(Career career) {
 		this.career = career;
 	}
-	
+
 	public void setCollege(boolean college) {
 		this.college = college;
 	}
-	
+
 	public boolean getCollege() {
 		return college;
 	}
@@ -115,6 +122,7 @@ public class Player {
 					boolean specialPopup = false;
 					count--;
 					player.setTile(player.getTile() + 1);
+
 					if (player.getTile() == 1232) {
 						Thread t = new Thread(new Runnable() {
 							public void run() {
@@ -138,7 +146,8 @@ public class Player {
 					} else if (path.get(player.getTile()) instanceof ChoiceTile) {
 						count = 0;
 					}
-					if ((path.get(player.getTile()) instanceof PayDayTile)) {
+
+					if (count == 0 && (path.get(player.getTile()) instanceof PayDayTile)) {
 						career.setSalary((int) (career.getSalary() * 1.05));
 					}
 					if (count == 0 && !specialPopup) {
@@ -146,14 +155,14 @@ public class Player {
 					}
 				}
 			}
-		}).start();			
+		}).start();
 	}
 
-    public int getDestination() {
-        return destination;
-    }
+	public int getDestination() {
+		return destination;
+	}
 
-    public void setDestination(int destination) {
-        this.destination = destination;
-    }
+	public void setDestination(int destination) {
+		this.destination = destination;
+	}
 }
