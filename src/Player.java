7dd6ffd3c1bf9a8c70cb92078966caae5c8b0c3e
ericlong2 <43346646+ericlong2@users.noreply.github.com>
2019-01-18 00:14:18 -0500

    move

--- src/Player.java
+++ src/Player.java
@@ -16,6 +16,9 @@ public class Player {
 	private boolean college;
 	private int add;
 	private int count;
+	private int x,y;
+	private double xDiff, yDiff;
+	private int step;
 
 	Player(String name, double balance, int x, int y) {
 		this.name = name;
@@ -25,8 +28,19 @@ public class Player {
 		destination = 0;
 		add = 0;
 		count = 0;
+		this.x = x;
+		this.y = y;
+		step = 0;
 	}
-
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -116,42 +130,51 @@ public class Player {
 
 	public void move(int spin, ArrayList<ActionTile> path) {
 		count += spin;
-		new Timer(1000, new ActionListener() {
+		step = 5;
+		xDiff = -(double)(x - path.get(player.getTile() + 1).getX()) / 5;
+		yDiff = -(double)(y - path.get(player.getTile() + 1).getY()) / 5;
+		new Timer(100, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (count > 0) {
-					boolean specialPopup = false;
-					count--;
-					player.setTile(player.getTile() + 1);
-
-					if (player.getTile() == 1232) {
-						Thread t = new Thread(new Runnable() {
-							public void run() {
-								new CareerPopUp(college, player);
-							}
-						});
-						t.start();
-						specialPopup = true;
-						count = 0;
-					} else if (player.getTile() == 2) {
-						Thread t = new Thread(new Runnable() {
-							public void run() {
-								new HouseSelectionPopUp(player);
-							}
-						});
-						t.start();
-						specialPopup = true;
-						count = 0;
-					} else if (path.get(player.getTile()) instanceof PayDayTile) {
-						player.addMoney(career.getSalary());
-					} else if (path.get(player.getTile()) instanceof ChoiceTile) {
-						count = 0;
-					}
-
-					if (count == 0 && (path.get(player.getTile()) instanceof PayDayTile)) {
-						career.setSalary((int) (career.getSalary() * 1.05));
-					}
-					if (count == 0 && !specialPopup) {
-						new PopUp(path.get(player.getTile()).getMessage(), path.get(player.getTile()), player);
+					if (step >= 0) {
+						step--;
+						x = (int)(xDiff + x);
+						y = (int)(yDiff + y);
+					} else {
+						step = 5;
+						boolean specialPopup = false;
+						count--;
+						player.setTile(player.getTile() + 1);
+						if (player.getTile() == 1232) {
+							Thread t = new Thread(new Runnable() {
+								public void run() {
+									new CareerPopUp(college, player);
+								}
+							});
+							t.start();
+							specialPopup = true;
+							count = 0;
+						} else if (player.getTile() == 2) {
+							Thread t = new Thread(new Runnable() {
+								public void run() {
+									new HouseSelectionPopUp(player);
+								}
+							});
+							t.start();
+							specialPopup = true;
+							count = 0;
+						} else if (path.get(player.getTile()) instanceof PayDayTile) {
+							player.addMoney(career.getSalary());
+						} else if (path.get(player.getTile()) instanceof ChoiceTile) {
+							count = 0;
+						}
+	
+						if (count == 0 && (path.get(player.getTile()) instanceof PayDayTile)) {
+							career.setSalary((int) (career.getSalary() * 1.05));
+						}
+						if (count == 0 && !specialPopup) {
+							new PopUp(path.get(player.getTile()).getMessage(), path.get(player.getTile()), player);
+						}
 					}
 				}
 			}
@@ -166,3 +189,4 @@ public class Player {
 		this.destination = destination;
 	}
 }
+             
