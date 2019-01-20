09c96077bd1bc0ba6925d2faaa9f5c30ad077883
Chris Xie <cx.chrisxie@gmail.com>
2019-01-20 15:20:48 -0500

    comment career, career selection, choicetile, client

--- src/ActionTile.java
+++ src/ActionTile.java
@@ -2,13 +2,20 @@
  * Name: ActionTile.java
  * Version: 1.0
  * Authors: Chris, Dora, Eric, Jason
- * Date: January 2nd, 2019
+ * Date: January 2, 2019
  */
 abstract public class ActionTile {
     private int x;
     private int y;
     private String message;
 
+    /**
+     * Constructor
+     *
+     * @param x       the x coordinate
+     * @param y       the y coordinate
+     * @param message the tile message
+     */
     ActionTile(int x, int y, String message) {
         this.x = x;
         this.y = y;
@@ -28,7 +35,7 @@ abstract public class ActionTile {
     /**
      * Sets x value
      *
-     * @param x
+     * @param x the x value
      */
     public void setX(int x) {
         this.x = x;
@@ -46,24 +53,25 @@ abstract public class ActionTile {
     /**
      * Sets y value
      *
-     * @param y
+     * @param y the y value
      */
     public void setY(int y) {
         this.y = y;
     }
 
-    /*
+    /**
      * Gets message
-     * @return mesage
+     *
+     * @return message
      */
-    private String getMessage() {
+    public String getMessage() {
         return message;
     }
 
     /**
      * Sets message
      *
-     * @param message
+     * @param message the tile message
      */
     public void setMessage(String message) {
         this.message = message;
