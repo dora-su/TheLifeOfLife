12d5663bd27bb9237a7d78f49999064fd31a3407
jason98wang <38818672+jason98wang@users.noreply.github.com>
2019-01-21 02:09:49 -0500

    polayer

--- src/Player.java
+++ src/Player.java
@@ -133,109 +133,115 @@ class Player {
 	}
 
 	public void move(Game g, int spin, ArrayList<ActionTile> path, boolean popUp) {
-        count += spin;
-        new Timer(500, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (count > 0) {
-                    boolean specialPopup = false;
-                    count--;
+		count += spin;
+		new Timer(500, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (count > 0) {
+					boolean specialPopup = false;
+					count--;
 
-                    //going on the next part of the map
-                    if (player.tile == 17) {
-                        player.setTile(35 + 1);
-                    } else if (player.tile == 66) {
-                        player.setTile(75 + 1);
-                    } else if (player.tile == 93) {
-                        player.tile = 106;
-                    } else {
-                        player.setTile(player.getTile() + 1);
-                    }
-                    if (player.getTile() == 44 || player.getTile() == 70 || player.getTile() == 76 || player.getTile() == 96 || player.getTile() == 104 || player.getTile() == 113) {
-                        player.setChild(player.getChild() + 1);
-                    } else if (player.getTile() == 35) {
-                        Thread t = new Thread(new Runnable() {
-                            public void run() {
-                                if (popUp)
-                                    new CareerSelection(false, player); //if land on a certain tile allow the player to choose career
-                                g.turn++;
-                            }
-                        });
-                        t.start();
-                        specialPopup = true;
-                        count = 0;
-                    } else if (player.getTile() == 50) {
-                    	
-                        Thread t2 = new Thread(new Runnable() {
-                            public void run() {
-                                if (popUp)
-                                    new HouseSelection(player); //if land on this specific tile create house selections choices for user
-                                g.turn++;
-                            }
-                        });
-                        t2.start();
+					//going on the next part of the map
+					if (player.tile == 17) {
+						player.setTile(35 + 1);
+					} else if (player.tile == 66) {
+						player.setTile(75 + 1);
+					} else if (player.tile == 93) {
+						player.tile = 106;
+					} else {
+						player.setTile(player.getTile() + 1);
+					}
 
-                        specialPopup = true;
-                        count = 0;
-                    } else if (player.tile == 42) {
-                        if (popUp) {
-                            new PopUp(player); //if land on this specific tile the user gets married
-                            family++;
-                        }
-                        specialPopup = true;
-                        count = 0;
-                    } else if (player.tile == 75) {
-                        Thread t = new Thread(new Runnable() {
-                            public void run() {
-                                if (popUp)
-                                    new CareerSelection(true, player);
-                                g.turn++;
-                            }
-                        });
-                        t.start();
-                        specialPopup = true;
-                        count = 0;
-                    } else if (path.get(player.getTile()) instanceof PayDayTile) {
-                        player.addMoney(career.getSalary()); //if player pass pay day tile add money to their bank balance
-                    } else if (path.get(player.getTile()) instanceof ChoiceTile) {
-                        count = 0; //if the player lands on a choice tile end their turn immediately
-                    }
+					if (player.getTile() == 35) {
+						Thread t = new Thread(new Runnable() {
+							public void run() {
+								if (popUp)
+									new CareerSelection(false, player); //if land on a certain tile allow the player to choose career
+								g.turn++;
+							}
+						});
+						t.start();
+						specialPopup = true;
+						count = 0;
+					} else if (player.getTile() == 50) {
+						Thread t2 = new Thread(new Runnable() {
+							public void run() {
+								if (popUp)
+									new HouseSelection(player); //if land on this specific tile create house selections choices for user
+								g.turn++;
+							}
+						});
+						t2.start();
 
-                    //if the tile the player lands on is a pay day tile, increase their salary
-                    if (count == 0 && (path.get(player.tile) instanceof PayDayTile)) {
-                        career.setSalary((int) (career.getSalary() * 1.05));
-                        g.turn++;
-                    }
-                    
-                    if(count == 0 && (path.get(player.tile) instanceof MoneyTile)) {
-                    	int money = ((MoneyTile) (path.get(player.tile))).getMoney();
-                    	if(money > 0) {
-                    		player.addMoney(money);
-                    	}else if(money < 0){
-                    		player.removeMoney(money * -1);
-                    	}
-                    }
-                    
-                    if (count == 0 && player.tile == 3) {
-                        Thread t = new Thread(new Runnable() {
-                            public void run() {
-                                if (popUp)
-                                    new CareerSelection(true, player);
-                                g.turn++;
-                            }
-                        });
-                        t.start();
-                        specialPopup = true;
-                    }
+						specialPopup = true;
+						count = 0;
+					} else if (player.tile == 42) {
+						if (popUp) {
+							new PopUp(player); //if land on this specific tile the user gets married
+							family++;
+						}
+						specialPopup = true;
+						count = 0;
+					} else if (player.tile == 75) {
+						Thread t = new Thread(new Runnable() {
+							public void run() {
+								if (popUp)
+									new CareerSelection(true, player);
+								g.turn++;
+							}
+						});
+						t.start();
+						specialPopup = true;
+						count = 0;
+					} else if (path.get(player.getTile()) instanceof PayDayTile) {
+						player.addMoney(career.getSalary()); //if player pass pay day tile add money to their bank balance
+					} else if (path.get(player.getTile()) instanceof ChoiceTile) {
+						count = 0; //if the player lands on a choice tile end their turn immediately
+					}
 
-                    System.out.println("Speical Pop pup" + specialPopup);
-                    //create a pop up with instructions if no special pop ups have been made already
-                    if (count == 0 && !specialPopup && popUp) {
-                        new PopUp(path.get(player.getTile()).getMessage(), path.get(player.getTile()), player);
-                    }
+					//if the tile the player lands on is a pay day tile, increase their salary
+					if (count == 0 && (path.get(player.tile) instanceof PayDayTile)) {
+						career.setSalary((int) (career.getSalary() * 1.05));
+						g.turn++;
+					}
 
-                }
-            }
-        }).start();
-    }
+					if (count == 0 && (path.get(player.tile) instanceof MoneyTile)) {
+						int money = ((MoneyTile) (path.get(player.tile))).getMoney();
+						if (money > 0) {
+							player.addMoney(money);
+						} else if (money < 0) {
+							player.removeMoney(money * -1);
+						}
+					}
+
+					System.out.println("player Tile " + player.tile);
+					if (count == 0 && player.tile == 2) {
+						Thread t = new Thread(new Runnable() {
+							public void run() {
+								if (popUp)
+									new CareerSelection(true, player);
+								g.turn++;
+							}
+						});
+						t.start();
+						specialPopup = true;
+					}
+
+					if (player.getTile() == 44 || player.getTile() == 70 || player.getTile() == 76
+							|| player.getTile() == 96 || player.getTile() == 104 || player.getTile() == 113) {
+						if (count == 0) {
+							player.setChild(player.getChild() + 1);
+						}
+					}
+
+					//System.out.println("Speical Pop pup" + specialPopup);
+					//create a pop up with instructions if no special pop ups have been made already
+					if (count == 0 && !specialPopup && popUp) {
+						new PopUp(path.get(player.getTile()).getMessage(), path.get(player.getTile()), player);
+					}
+
+				}
+			}
+		}).start();
+	}
 
 }
