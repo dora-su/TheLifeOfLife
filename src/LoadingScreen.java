1ce4a490468bd8493f40ae2c13a445f374a4deda
Chris Xie <cx.chrisxie@gmail.com>
2019-01-21 21:15:07 -0500

    loading screen update + comments

--- src/LoadingScreen.java
+++ src/LoadingScreen.java
@@ -16,10 +16,16 @@ public class LoadingScreen extends JFrame {
     static JProgressBar loadingBar;
     static DefaultBoundedRangeModel model;
     private boolean loaded = false;
-    
+
+    /**
+     * Constructor
+     *
+     * @param time the time to load
+     */
     LoadingScreen(int time) {
-        setSize(500, 300);
+        setSize(500, 10);
         this.setLocation((int) (Game.screenX / 2) - 250, ((int) (Game.screenY / 2) - 150));
+        this.setUndecorated(true);
         model = new DefaultBoundedRangeModel(0, time, 0, time);
         loadingBar = new JProgressBar(model);
         add(loadingBar);
@@ -38,10 +44,20 @@ public class LoadingScreen extends JFrame {
 
     }
 
+    /**
+     * Gets if the bar has loaded
+     *
+     * @return loaded
+     */
     public boolean getLoaded() {
         return loaded;
     }
 
+    /**
+     * Sets the value of loaded
+     *
+     * @param loaded if it has loaded
+     */
     public void setLoaded(boolean loaded) {
         this.loaded = loaded;
     }
